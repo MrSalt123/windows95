cfb57e2b405a7eca030b6e1a429299b27a9f212f
Noah Nielsen <njn04807@uga.edu>
2024-12-19 16:42:27 -0500

    task bar changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { MenuList, MenuListItem, Separator, styleReset, Button } from 'react95';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import React, { useState, useEffect } from "react";
+import { Button, styleReset, MenuListItem } from "react95";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import win95Logo from "./assets/images/win95.png";
+import win95Computer from "./assets/images/computer.png";
+import bgImage from "./assets/images/windowsbackground.jpg"
+import recycle from "./assets/images/recycle.png"
 
 /* Pick a theme of your choice */
-import original from 'react95/dist/themes/original';
+import original from "react95/dist/themes/original";
 
 /* Original Windows95 font (optional) */
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
 
-const bgImage = require('./assets/images/windows95bglogo.png');
 
 const GlobalStyles = createGlobalStyle`
   ${styleReset}
@@ -36,6 +38,10 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const handleClick = () => {
+    console.log('clicked');
+}
+
 const App = () => {
     const [time, setTime] = useState("");
 
@@ -54,23 +60,58 @@ const App = () => {
         <div>
             <GlobalStyles />
             <ThemeProvider theme={original}>
-                <div className="fixed bottom-0 !w-[100vw]">
-                    {/* Left side menu */}
-                    <MenuList className="flex items-center gap-2" inline>
-                        <MenuListItem square>
+                <div className="ml-3 mt-3">
+                    <img
+                        src={win95Computer}
+                        alt="My Computer Icon"
+                        style={{ height: "5vh", marginBottom: "5px" }}
+                        onClick={handleClick}
+                    />
+                    <span style={{ fontSize: "0.8rem", textAlign: "center" }}>My Computer</span>
+                    
+                    <img
+                        src={recycle}
+                        alt="My Computer Icon"
+                        style={{ height: "5vh", marginBottom: "5px" }}
+                    />
+                    <span style={{ fontSize: "0.8rem", textAlign: "center" }}>Recycle</span>
+                </div>
+                {/* Taskbar */}
+                <div
+                    style={{
+                        position: "fixed",
+                        bottom: 0,
+                        width: "100vw",
+                        height: "40px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        backgroundColor: "#c0c0c0",
+                        borderTop: "2px solid #ffffff",
+                        borderBottom: "2px solid #808080",
+                    }}
+                >
+                    {/* Left side: Start Button and Menu Items */}
+                    <div style={{ display: "flex", alignItems: "center", paddingLeft: "5px" }}>
+                        <Button style={{ fontWeight: "bold", marginRight: "10px" }}>
                             <img
                                 src={win95Logo}
                                 alt="Start Icon"
-                                className="h-6 mr-2"
+                                style={{ height: "20px", marginRight: "5px" }}
                             />
-                        </MenuListItem>
-                        <Button>Roadmap</Button>
-                        <Button className='ml-6'>About</Button>
-                        <Button className='ml-6 mr-6'>Contact</Button>
-                        <Button className="font-ms-sans-serif text-sm">
-                            {time}
+                            Start
                         </Button>
-                    </MenuList>
+                        <div style={{ display: "flex", gap: "15px" }}>
+                            <MenuListItem style={{ fontWeight: "bold", cursor: "pointer" }} onClick={handleClick}>Roadmap</MenuListItem>
+                            <MenuListItem style={{ fontWeight: "bold", cursor: "pointer" }}>About</MenuListItem>
+                            <MenuListItem style={{ fontWeight: "bold", cursor: "pointer" }}>Contact</MenuListItem>
+                        </div>
+                    </div>
+
+                    {/* Right side: Clock */}
+                    <div style={{ paddingRight: "10px" }}>
+                        <Button className="font-bold" active>{time}</Button>
+                    </div>
                 </div>
             </ThemeProvider>
         </div>
